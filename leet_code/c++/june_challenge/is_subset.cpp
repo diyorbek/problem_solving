b137f979d078c043d5016ce3ac9b42c7fcdd0675
Diyorbek Sadullaev <diyorbek3377@gmail.com>
2020-06-09 21:09:04 +0200

    June 9, 2020

---
+++ leet_code/c++/june_challenge/is_subset.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+ public:
+  bool isSubsequence(string s, string t) {
+    int j = 0;
+
+    for (int i = 0; i < t.length(); i++) {
+      if (t[i] == s[j]) {
+        j++;
+      }
+    }
+
+    return j == s.length();
+  }
+};
